b303b6ff09f28779444f20aeb4c62bb03d488516
Abdullah Hossien <96534430+itzabd@users.noreply.github.com>
2025-07-28 08:48:46 +0600

    Initial commit

---
+++ test_install.py
@@ -0,0 +1,8 @@
+# test_install.py
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from flask import Flask
+
+print("All packages imported successfully!")
